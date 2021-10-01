c2c259aa52ac940e5913b9984454d9e6bbd53e33
devtechie <devtechieinteractive@gmail.com>
2021-10-01 12:01:08 -0700

    Initial Commit

---
+++ GoalsApp/GoalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsAppApp.swift
+//  GoalsApp
+//
+//  Created by DevTechie on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
